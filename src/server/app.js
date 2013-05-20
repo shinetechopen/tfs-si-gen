d3a9c954787a4c23edee5647cbb43f30136981eb
Lu Rongkai <lurongkai@gmail.com>
2013-05-20 23:26:28 +0800

    project structure.

---
+++ src/server/app.js
@@ -0,0 +1,10 @@
+///<reference path='../libs/node.d.ts'/>
+var http = require("http")
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+//@ sourceMappingURL=app.js.map
---
+++ src/server/app.ts
@@ -0,0 +1,10 @@
+///<reference path='../libs/node.d.ts'/>
+
+import http = module("http");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
