63ca85b4457a59e3f39fc1a402e1f003b7e5c995
Lu Rongkai <lurongkai@gmail.com>
2013-05-23 14:18:01 +0800

    add tfs service interface.

---
+++ src/server/CSharpInteropLib/ITfsWorkItemService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInteropLib
+{
+    public interface ITfsWorkItemService
+    {
+        /// <summary>
+        /// Create new support issue in tfs.
+        /// </summary>
+        /// <param name="param">support issue detail</param>
+        /// <returns>SI number</returns>
+        string CreateNewSupportIssue(dynamic param, params Stream[] files);
+
+        /// <summary>
+        /// Close support issue with User Information.
+        /// </summary>
+        /// <param name="ticketNumber">ticket number</param>
+        void CloseSupportWithUserInformation(string ticketNumber);
+
+        /// <summary>
+        /// Close Support issue and open new bug.
+        /// </summary>
+        /// <param name="ticketNumber">ticket number</param>
+        /// <returns>Bug number</returns>
+        string CloseSupportAndCreateBug(string ticketNumber);
+    }
+}
---
+++ src/server/CSharpInteropLib/TfsWorkItemService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInteropLib
+{
+    public class TfsWorkItemService : ITfsWorkItemService
+    {
+        public string CreateNewSupportIssue(dynamic param, params System.IO.Stream[] files) {
+            throw new NotImplementedException();
+        }
+
+        public void CloseSupportWithUserInformation(string ticketNumber) {
+            throw new NotImplementedException();
+        }
+
+        public string CloseSupportAndCreateBug(string ticketNumber) {
+            throw new NotImplementedException();
+        }
+    }
+}
